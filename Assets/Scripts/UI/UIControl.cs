8309d4f6a331ee00a8dc5518871088c87b5b15f3
CalebWAC <calebwac@outlook.com>
2022-07-20 10:40:58 -0600

    gamepad hotbar control

--- Assets/Scripts/UI/UIControl.cs
+++ Assets/Scripts/UI/UIControl.cs
@@ -7,7 +7,28 @@ public class UIControl : MonoBehaviour
 {
     public int usesRemaining = 5;
     public GameObject verseUI;
-    private bool UIOpen = false;  
+    private bool UIOpen = false; 
+
+    private GameObject selector;
+
+    void Start() {
+        selector = GameObject.Find("/Canvas/SelectionBarWeapon/Selector");
+    }
+
+    // Moves selector for active verse
+    void Update() {
+        if (Keyboard.current.digit1Key.wasPressedThisFrame) {
+            selector.GetComponent<RectTransform>().anchoredPosition = new Vector3(-0.3332977f, 4.095f, 0);
+        } else if (Keyboard.current.digit2Key.wasPressedThisFrame) {
+            selector.GetComponent<RectTransform>().anchoredPosition = new Vector3(-0.3332977f, 1.750061f - 0.030061f, 0);
+        } else if (Keyboard.current.digit3Key.wasPressedThisFrame) {
+            selector.GetComponent<RectTransform>().anchoredPosition = new Vector3(-0.3332977f, -0.624939f - 0.030061f, 0);
+        } else if (Keyboard.current.digit4Key.wasPressedThisFrame) {
+            selector.GetComponent<RectTransform>().anchoredPosition = new Vector3(-0.3332977f, -2.999939f - 0.030061f, 0);
+        } else if (Keyboard.current.digit5Key.wasPressedThisFrame) {
+            selector.GetComponent<RectTransform>().anchoredPosition = new Vector3(-0.3332977f, -5.374954f - 0.030061f, 0);
+        }
+    }
 
     void OnMenu() 
     {
@@ -24,6 +45,25 @@ public class UIControl : MonoBehaviour
             Cursor.visible = false;
             // Cursor.lockState = CursorLockMode.Locked;
             Screen.lockCursor = true;
+        }
+    }
+
+    // Moves selector for gamepad
+    void OnSwitch(InputValue value)
+    {
+        float lr = value.Get<float>();
+
+        if (lr == 1) {
+            Debug.Log("Ich bin hier");
+            selector.GetComponent<RectTransform>().anchoredPosition = new Vector3(-0.3332977f, selector.GetComponent<RectTransform>().anchoredPosition.y + 2.375f, 0);
+        } else if (lr == -1) {
+            selector.GetComponent<RectTransform>().anchoredPosition = new Vector3(-0.3332977f, selector.GetComponent<RectTransform>().anchoredPosition.y + -2.375f, 0);
+        }
+
+        if (selector.GetComponent<RectTransform>().anchoredPosition.y > 4.095f) {
+            selector.GetComponent<RectTransform>().anchoredPosition = new Vector3(-0.3332977f, -5.374954f - 0.030061f, 0);
+        } else if (selector.GetComponent<RectTransform>().anchoredPosition.y < -6f) {
+            selector.GetComponent<RectTransform>().anchoredPosition = new Vector3(-0.3332977f, 4.095f, 0);
         }
     }
 }
